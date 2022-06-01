1ee15cbbc8a5a8c580a6c41b472b17043b843313
Betül Gülcen <betulgulcen@stu.comu.edu.tr>
2022-06-01 19:57:32 +0300

    Add files via upload

---
+++ orn2-kullanicigirisi/orn2-kullanicigirisi/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  orn2-kullanicigirisi
+//
+//  Created by Betül Gülcen on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        // kullanıcı adı ve parola girilsin
+//        parola ve kullanıcı adı doğru ise başarılı değilse başarısız sonucu döndürülsün
+        
+    }
+
+    @IBAction func btnCalculator(_ sender: Any) {
+        
+//        butona tıkladıktan sonra çalışacak olan kısım.
+        
+        let username = "betul"
+        let password = "1234"
+        
+        if username == text1.text && password == text2.text{
+            lblResult.text = "Giriş başarılı"
+        }else{
+            lblResult.text = "Kullanıcı adı ya da parola hatalı."
+        }
+    }
+    
+}
+
